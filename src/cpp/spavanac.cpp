97d28642c4347e5dd73712f25cbaa694784998f1
Birkhoff Lee <birkhoff.lee.cn@gmail.com>
2016-11-05 11:11:10 +0800

    add solution for spavanac, update gitignore

---
+++ src/cpp/spavanac.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int H, M;
+
+	cin >> H >> M;
+
+	if (M < 45) {
+		H--;
+
+		if (H < 0) {
+			H += 24;
+		}
+
+		M += 60;
+	}
+	
+	M -= 45;
+
+	cout << H << " " << M << endl;
+
+	return 0;
+}
